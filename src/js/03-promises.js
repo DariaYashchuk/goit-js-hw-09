6833c9118fcccd7daaf45ebd03fd855bb5ecebae
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-05-23 17:18:45 +0300

    1111111

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,55 +3,45 @@ const refs = {
   delayStep: document.querySelector('[name="step"]'),
   amount: document.querySelector('[name="amount"]'),
 
+  form: document.querySelector('form'),
+
   btnCreate: document.querySelector('button'),
 };
 
 // console.log(refs.btnCreate);
 
-function onStart(e) {
+function onFormSubmit(e) {
   e.preventDefault();
+
   const position = Number(refs.amount.value);
-  const delay = Number(refs.firstDelay.value);
+  let delay = Number(refs.firstDelay.value);
   const step = Number(refs.delayStep.value);
-  // console.log(delay);
 
   for (let i = 1; i <= position; i += 1) {
-    // console.log(delay);
-    function createPromise(position, delay) {
-      delay += step * i;
-      return new Promise((res, rej) => {
-        setTimeout(() => {
-          const shouldResolve = Math.random() > 0.3;
-          if (shouldResolve) {
-            res('+');
-          } else {
-            rej('-');
-          }
-        }, delay);
-      });
-    }
-    // console.log(i);
     createPromise(position, delay)
-      .then(({ position, delay }) => {
+      .then(({ i, delay }) => {
         console.log(`✅ Fulfilled promise ${i} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${i} in ${delay}ms`);
       });
+    delay += step;
   }
+
+  e.currentTarget.reset();
 }
 
-// function createPromise(position, delay) {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       const shouldResolve = Math.random() > 0.3;
-//       if (shouldResolve) {
-//         res('+');
-//       } else {
-//         rej('-');
-//       }
-//     }, delay);
-//   });
-// }
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
 
-refs.btnCreate.addEventListener('click', onStart);
+refs.form.addEventListener('submit', onFormSubmit);
